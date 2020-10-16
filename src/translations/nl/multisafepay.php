14a486063b99642ec4dbf16cccba28cba29b1abd
Joris <63226870+Joris-MultiSafepay@users.noreply.github.com>
2020-10-16 12:25:52 +0200

    Add Dutch translations

---
+++ src/translations/nl/multisafepay.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+return [
+    'MultiSafepay Settings' => 'MultiSafepay instellingen',
+    'General' => 'Algemeen',
+    'Settings' => 'Instellingen',
+    'Order status settings' => 'Bestelling status instellingen',
+    'Test mode' => 'Test modus',
+    'Order status when shipped' => 'Order status na verzending',
+    'MultiSafepay Support' => 'MultiSafepay Support',
+    'Documentation' => 'Documentatie',
+    'Read our documentation for more information about MultiSafepay and how to get started' => 'Lees onze documentatie voor meer informatie over MultiSafepay en hoe je kan beginnen',
+    'Manual' => 'Handleiding',
+    'Changelog' => 'Changelog',
+    'FAQ' => 'FAQ',
+    'For developers' => 'Voor ontwikkelaars',
+    'API Documentation' => 'API Documentatie',
+    'MultiSafepay Github' => 'MultiSafepay Github',
+    'Account' => 'Account',
+    'To use this plugin you need a MultiSafepay account' => 'Om deze plugin te gebruiken is een MultiSafepay account vereist',
+    'If you would like to have a clear overview of what MultiSafepay has to offer, feel free to create a' => 'Als je meer te weten wilt komen over wat MultiSafepay te bieden heeft, maak dan een',
+    'test account' => 'test account',
+    'If you would like to set up a live account, please contact the MultiSafepay sales department' => 'Als je een live account wilt opzetten, contacteer dan de MultiSafepay sales afdeling',
+    'Telephone' => 'Telefoon',
+    'E-mail' => 'E-mail',
+    'Contact' => 'Contact',
+    'Need assistance? Feel free to contact our integration team' => 'Hulp nodig? Neem dan gerust contact op met ons integration team',
+];
